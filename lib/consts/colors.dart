159fdf0a0e58b3692526466d004094cc65316aa9
Cyb3r7ron <kiduskyle@gmail.com>
2022-01-12 06:42:21 -0500

    last

---
+++ lib/consts/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ColorsConsts {
+  static Color black = Color(0xFF000000);
+  static Color white = Color(0xFFFFFFFF);
+  static Color title = Color(0xDD000000);
+  static Color subTitle = Color(0x8A000000);
+  static Color backgroundColor = Color(0xFFFAFAFA); //grey shade 50
+
+  static Color favColor = Color(0xFFF44336); // red 500
+  static Color favBadgeColor = Color(0xFFE57373); // red 300
+
+  static Color cartColor = Color(0xFFFF5722); //deep orange
+  static Color cartBadgeColor = Color(0xFFFF3D00); //deep orange accent400
+
+  static Color gradiendFStart = Color(0xFFF57C00); //orange700
+  static Color gradiendFEnd = Color(0xFFE1BEE7); //purple 100
+  static Color endColor = Color(0xFFEF6C00); //orange 800
+  static Color orange300 = Color(0xFFFFB74D); //orange 300
+  static Color gradiendLEnd = Color(0xFFFB8C00); //orange 600
+  static Color gradiendLStart = Color(0xFFF57C00); //orange 700
+  static Color starterColor = Color(0xFFFF9100); //orangeaccent 400
+  static Color amber900 = Color(0xFFFF6F00);
+}
